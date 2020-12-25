d7a342abe8b761232c3c852041e52b435033b24a
Ali Raza <sp19-bse-001@students.cuisahiwal.edu.pk>
2020-12-25 19:34:29 +0500

    edit login.fxml and create dashboard package

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -1,5 +1,32 @@
 package main;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Controller {
 
+    @FXML
+    public Button login_btn;
+
+    public void login(ActionEvent actionEvent) throws IOException {
+        ((Stage) login_btn.getScene().getWindow()).close();
+        Login_Click();
+    }
+
+    public void Login_Click() throws IOException {
+        Parent Layout = FXMLLoader.load(getClass().getResource("/dashboard/dashboard.fxml"));
+        Stage stage = new Stage();
+        stage.setScene(new Scene(Layout));
+        stage.setMaximized(true);
+        stage.setTitle("Pharmacy Management");
+        stage.show();
+    }
 }
