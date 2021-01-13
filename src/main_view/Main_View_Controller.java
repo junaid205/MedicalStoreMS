b66900024c75be30c244561b5ca0131727bd5352
Ali Raza <sp19-bse-001@students.cuisahiwal.edu.pk>
2021-01-13 18:08:57 +0500

    All Updated

--- src/main_view/Main_View_Controller.java
+++ src/main_view/Main_View_Controller.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Main_View_Controller implements Initializable {
+public class Main_View_Controller  {
     public BorderPane allView;
     @FXML
 
