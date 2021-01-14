b21dee8a68a97de11f3d1f5cf434f81be26a497f
Ali Raza <sp19-bse-001@students.cuisahiwal.edu.pk>
2021-01-14 13:26:03 +0500

    All Updated

--- src/module2/AddMedicine_Controller.java
+++ src/module2/AddMedicine_Controller.java
@@ -2,14 +2,19 @@ package module2;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
+
+
+import java.io.IOException;
 
 public class AddMedicine_Controller {
     @FXML
-    private Button Exit_Btn;
+    private BorderPane AllView;
 
-    public void Close(ActionEvent actionEvent) {
-        ((Stage) Exit_Btn.getScene().getWindow()).close();
+    public void ManageMedicine(ActionEvent actionEvent) throws IOException {
+        Parent layout = FXMLLoader.load(getClass().getResource("/module2/Medicines.fxml"));
+        AllView.setCenter(layout);
     }
 }
--- src/module2/Medicines_Controller.java
+++ src/module2/Medicines_Controller.java
@@ -1,21 +1,20 @@
 package module2;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+import javafx.scene.layout.BorderPane;
+
 
 import java.io.IOException;
 
 public class Medicines_Controller {
+    @FXML
+    private BorderPane AllView;
 
     public void AddMedicine(ActionEvent actionEvent) throws IOException {
         Parent layout = FXMLLoader.load(getClass().getResource("/module2/AddMedicine.fxml"));
-        Stage stage = new Stage();
-        stage.setScene(new Scene(layout));
-        stage.initStyle(StageStyle.UNDECORATED);
-        stage.show();
+        AllView.setCenter(layout);
     }
 }
